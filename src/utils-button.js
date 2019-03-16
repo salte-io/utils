915671b25625dfa539122e854bfc4c3373c0b939
Ceci <admin@cecilias.me>
2019-03-16 12:28:15 -0500

    Initial commit

---
+++ src/utils-button.js
@@ -0,0 +1,67 @@
+import { LitElement, html, css, customElement } from 'lit-element';
+
+@customElement('utils-button')
+class Button extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        padding: 5px 20px;
+        border-radius: 3px;
+        cursor: pointer;
+
+        color: #fafafa;
+        border: 1px solid #fafafa;
+
+        transition: 0.15s ease-in-out;
+        transition-property: background-color, color;
+      }
+
+      :host(:hover),
+      :host([checked]) {
+        background: #fafafa;
+        color: #1B1D23;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+
+  static get properties() {
+    return {
+      checked: {
+        type: Boolean,
+        reflect: true
+      },
+
+      checkable: {
+        type: Boolean,
+        reflect: true
+      }
+    };
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('checkable')) {
+      if (this.checkable) {
+        this.addEventListener('click', this.onCheck);
+      } else {
+        this.removeEventListener('click', this.onCheck);
+      }
+    }
+  }
+
+  onCheck() {
+    this.checked = !this.checked;
+
+    const event = new CustomEvent('checked', {
+      detail: this.checkable
+    });
+    this.dispatchEvent(event);
+  }
+}
+
+export default Button;
---
+++ src/utils-cli.js
@@ -0,0 +1,126 @@
+import { LitElement, html, css, customElement } from 'lit-element';
+
+import hljs from 'highlight.js/lib/highlight';
+import shell from 'highlight.js/lib/languages/shell';
+hljs.registerLanguage('shell', shell);
+
+@customElement('utils-cli')
+class Terminal extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: flex;
+        flex: 1;
+        position: relative;
+      }
+
+      textarea {
+        color: transparent;
+        caret-color: white;
+        background: transparent;
+        margin: 2px 10px;
+        padding: 0;
+        resize: none;
+        border: none;
+        width: 100%;
+        outline: none;
+        font-family: inherit;
+        font-size: inherit;
+      }
+
+      #visual {
+        position: absolute;
+        top: -1px;
+        left: 10px;
+      }
+
+      /* HighlightJS Tomorrow Night */
+      .hljs{display:block;overflow-x:auto;padding:.5em;background:#1E1E1E;color:#DCDCDC}.hljs-addition,.hljs-deletion{display:inline-block;width:100%}.hljs-keyword,.hljs-link,.hljs-literal,.hljs-name,.hljs-symbol{color:#569CD6}.hljs-link{text-decoration:underline}.hljs-built_in,.hljs-type{color:#4EC9B0}.hljs-class,.hljs-number{color:#B8D7A3}.hljs-meta-string,.hljs-string{color:#D69D85}.hljs-regexp,.hljs-template-tag{color:#9A5334}.hljs-formula,.hljs-function,.hljs-params,.hljs-subst,.hljs-title{color:#DCDCDC}.hljs-comment,.hljs-quote{color:#57A64A;font-style:italic}.hljs-doctag{color:#608B4E}.hljs-meta,.hljs-meta-keyword,.hljs-tag{color:#9B9B9B}.hljs-template-variable,.hljs-variable{color:#BD63C5}.hljs-attr,.hljs-attribute,.hljs-builtin-name{color:#9CDCFE}.hljs-section{color:gold}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}.hljs-bullet,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-selector-pseudo,.hljs-selector-tag{color:#D7BA7D}.hljs-addition{background-color:#144212}.hljs-deletion{background-color:#600}
+    `;
+  }
+
+  render() {
+    return html`
+      <textarea
+        type="text"
+        spellcheck="false"
+        .value="${`$ ${(this.value || '').replace(/\$\s?/g, '')}`}"
+        @input="${({ target }) => {
+          const value = target.value.replace(/\$\s?/g, '');
+          this.value = value;
+          const event = new CustomEvent('change', {
+            detail: this.value
+          });
+          this.dispatchEvent(event);
+        }}">
+      </textarea>
+      <div id="visual"></div>
+    `;
+  }
+
+  static get properties() {
+    return {
+      value: String
+    };
+  }
+
+  constructor() {
+    super();
+
+    this.value = null;
+    this.commands = [];
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.addEventListener('keydown', this.onKeyDown);
+    this.addEventListener('click', this.onCursorChange);
+    this.addEventListener('focus', this.onCursorChange);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.removeEventListener('keydown', this.onKeyDown);
+    this.removeEventListener('click', this.onCursorChange);
+    this.removeEventListener('focus', this.onCursorChange);
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has('value')) {
+      this.visual.innerHTML = `$ ${hljs.highlight('shell', this.value || '').value}`;
+    }
+  }
+
+  get visual() {
+    if (!this._visual) {
+      this._visual = this.shadowRoot.getElementById('visual');
+    }
+
+    return this._visual;
+  }
+
+  onKeyDown(e) {
+    if (e.code === 'ArrowUp') {
+      e.preventDefault();
+    } else if (e.code === 'ArrowDown') {
+      e.preventDefault();
+    } else if (e.code === 'Enter') {
+      e.preventDefault();
+      const event = new CustomEvent('submit', {
+        detail: this.value
+      });
+      this.dispatchEvent(event);
+      this.value = null;
+    }
+
+    this.onCursorChange(e);
+  }
+
+  onCursorChange({ target }) {
+    target.selectionStart = Math.max(3, target.selectionStart);
+  }
+}
+
+export default Terminal;
