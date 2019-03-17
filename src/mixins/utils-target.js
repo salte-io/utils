0eac7610e677a7b4fa7533340404b1b348266702
Ceci <admin@cecilias.me>
2019-03-16 23:45:57 -0500

    chore: added the ability to quickly copy
    * fixed various issues with the terminal textarea

---
+++ src/mixins/utils-target.js
@@ -0,0 +1,14 @@
+const OriginalTargetMixin = function(superClass) {
+  return class extends superClass {
+    getOriginalTarget(ev) {
+      if ('composedPath' in ev) return ev.composedPath()[0]; // Standard
+      else if ('path' in ev) return ev.path[0]; // Old Chrome
+      else if ('originalTarget' in ev) return ev.originalTarget; // Firefox
+      else if ('srcElement' in ev) return ev.srcElement; // Old IE & Safari
+      else return ev.target; // Fallback to normal target.
+    }
+  };
+};
+
+export { OriginalTargetMixin };
+
