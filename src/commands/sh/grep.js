5550b663e5b5a447e3a1a900096e350e87b9bc41
Ceci <admin@cecilias.me>
2019-03-17 18:38:39 -0500

    feat: added a basic grep command
    * added support for clearing history
    * added support for color codes via chalk

---
+++ src/commands/sh/grep.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+
+chalk.enabled = true;
+chalk.supportsColor = true;
+chalk.level = 3;
+
+export default class CLI {
+  static get help() {
+    return {
+      usage: '[pattern]',
+      description: 'file pattern searcher'
+    };
+  }
+
+  static process(args, input) {
+    if (args._.length) {
+      // filter
+      return input.split('\n').filter((line) => {
+        return line.includes(args._.join(' '));
+      }).map((line) => {
+        return line.replace(args._.join(' '), (substring) => {
+          return `${chalk.redBright(substring)}${chalk.reset()}`;
+        });
+      }).join('\n');
+    }
+
+    return input;
+  }
+}
